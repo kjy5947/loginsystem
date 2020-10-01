4ce77c7949d773476846781ccb8748f44626f46d
kjy5947 <kjy5947@gmail.com>
2020-10-02 03:45:25 +0900

    정보보호내용_수정

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 const mongoose=require('mongoose');
 mongoose.connect(config.mongoURI,{
     useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false
-}).then(()=>console.log('MongDB Connected...'))
+}).then(()=>console.log('MongoDB Connected...'))
   .catch(err=>console.log(err))
 
 
